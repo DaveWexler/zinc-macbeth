5f0c2ea330f5561b900212535043b95ceac1dffe
DaveWexler <dwexler@gmail.com>
2017-02-13 16:51:33 -0500

    make method for count_lines

--- macbeth_analyser.rb
+++ macbeth_analyser.rb
@@ -3,15 +3,18 @@ require 'pry'
 require 'open-uri'
 
 xml = Nokogiri::XML(open("http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml"))
-char_lines_count = Hash.new(0)
 
-chars = xml.xpath("//SPEAKER")
-chars.map do |char|
-  unless char.text == "ALL"
-    char_lines_count[char.text] += char.xpath("../LINE").length
+def count_lines(xml_file)
+  chars = xml_file.xpath("//SPEAKER")
+  char_lines_count = Hash.new(0)
+  chars.map do |char|
+    unless char.text == "ALL"
+      char_lines_count[char.text] += char.xpath("../LINE").length
+    end
+  end
+  char_lines_count.each do |char, num|
+    puts "#{num} #{char}"
   end
 end
 
-char_lines_count.each do |char, num|
-  puts "#{num} #{char}"
-end
+count_lines(xml)
